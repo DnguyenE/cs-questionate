b153dd8245027f9adf0d0285321fe09e8e545fa6
brutalbeat <118843819+brutalbeat@users.noreply.github.com>
2023-01-05 16:38:15 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div>
+<<<<<<< Updated upstream
       <h1>Hello World</h1>
+=======
+    <h1>Welcome to Questionate</h1>
+    <h4>This is a digital study application that uses flashcards to help you study and learn!  </h4>
+    <h4>You can use one of our built-in flashcard sets, or create a set of custom flashcards to test yourself with</h4>
+    <h3>To get started, click the 'Create New Flashcard' box in the upper right corner of the page </h3>
+>>>>>>> Stashed changes
     </div>
   )
 }
