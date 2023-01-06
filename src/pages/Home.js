e6e3e3fc1ae5119b5944777d95629c50929ab7eb
brutalbeat <118843819+brutalbeat@users.noreply.github.com>
2023-01-05 17:20:13 -0800

    3newelements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
     <h4>You can use one of our built-in flashcard sets, or create a set of custom flashcards to test yourself with</h4>
     <h3>To get started, click the 'Create New Flashcard' box in the upper right corner of the page </h3>
     <h2>Buddy </h2>
+    <h5>dude</h5>
     </div>
   )
 }
