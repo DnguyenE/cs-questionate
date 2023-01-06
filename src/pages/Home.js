adf04fdffeb35a5deadd63aca102210479c1a5af
brutalbeat <118843819+brutalbeat@users.noreply.github.com>
2023-01-05 16:55:40 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     <h4>This is a digital study application that uses flashcards to help you study and learn!  </h4>
     <h4>You can use one of our built-in flashcard sets, or create a set of custom flashcards to test yourself with</h4>
     <h3>To get started, click the 'Create New Flashcard' box in the upper right corner of the page </h3>
+    <h2>Buddy </h2>
     </div>
   )
 }
