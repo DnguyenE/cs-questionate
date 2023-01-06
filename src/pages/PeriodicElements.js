211d52883e0d90e8a90ed270c5d12cf1d1da0714
brutalbeat <118843819+brutalbeat@users.noreply.github.com>
2023-01-05 17:19:50 -0800

    contentfiles+3

---
+++ src/pages/PeriodicElements.js
@@ -0,0 +1,14 @@
+export const ATOMIC_FLASHCARDS = [
+    {
+      id: 1,
+      question: 'What is 2 + 2?',
+      answer: '4',
+      options: ['2', '3', '4', '5'],
+    },
+    {
+      id: 2,
+      question: 'Question 2?',
+      answer: 'Answer',
+      options: ['Answer', 'Answer 1', 'Answer 2', 'Answer 3'],
+    },
+  ]
