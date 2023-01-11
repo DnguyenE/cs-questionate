97e0c0b4d09c2efef636ecb529fdf226fb3e2bc2
brutalbeat <118843819+brutalbeat@users.noreply.github.com>
2023-01-11 13:11:29 -0800

    some more about us page work

--- src/pages/FlashCards.js
+++ src/pages/FlashCards.js
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Button } from "@mui/material";
 
 export default function savedFlashCards() {
   return (
@@ -45,5 +45,5 @@ export default function savedFlashCards() {
         </Button>
       </form>
     </div>
-  )
+  );
 }
--- src/pages/aboutproject.js
+++ src/pages/aboutproject.js
@@ -1,5 +1,41 @@
-export default function AboutProject (){
-    return (
-        <h1>hello world</h1>
-    )
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+//import Container from '@mui/material/Container';
+export default function AboutProject() {
+  return (
+    <div>
+      <h1>About Our Project</h1>
+      <form></form>
+      <p>
+        Questionate is an application developed by Ethan Dinh and Emil
+        Melconian. Intended as a digital study tool, the goal during development
+        was to create a simple and easy-to-use application that helps students
+        memorize and learn efficiently. With two learning modes, students are
+        able to use a template to input custom information, or use one of our
+        offered study topics.
+      </p>
+      <h2 className="clickbelow">Click below to start learning now!</h2>
+      <form className="aboutusbutton1">
+        <Button
+          href="/create"
+          colour="primary"
+          size="medium"
+          variant="contained"
+        >
+          Create your own
+        </Button>
+      </form>
+      <h3 className="or">Or try our pre-made flashcards:</h3>
+      <form className="aboutusbutton2">
+        <Button
+          href="/saved-flashcards"
+          colour="primary"
+          size="medium"
+          variant="contained"
+        >
+          Pre-made flashcards
+        </Button>
+      </form>
+    </div>
+  );
 }
